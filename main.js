02fbbbdaf9f20087447fa20e13410ed16076908f
Jesús Antonio Pérez Sandoval <jasp402@gmail.com>
2020-08-12 16:28:51 -0500

    fix: correcion

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1400, height: 768, frame:false})
+  mainWindow = new BrowserWindow({width: 1400, height: 768})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -27,7 +27,7 @@ function createWindow () {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
-
+  mainWindow.setMenu(null);
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
